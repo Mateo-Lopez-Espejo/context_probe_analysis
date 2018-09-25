f5f66065f842455f37059323e7e1d5adf4620e4c
Mateo Lopez Espejo <lopezesp@ohsu.edu>
2018-09-24 19:20:09 -0700

    finish kruskall wallis main analysis and plotting. need to clean and reposition some functions within pipeline

--- CPP_pipeline.py
+++ CPP_pipeline.py
@@ -1,27 +1,24 @@
-
-import nems_db.baphy as nb
-import nems.recording as recording
-import cpp_epochs as cpe
 import joblib as jl
-import os
-import cpp_plots as cplt
-import cpp_PCA as cpca
 import matplotlib.pyplot as plt
 import numpy as np
-import nems.epoch as nep
-import cpp_dispersion as cdisp
-import cpp_parameter_handlers as hand
 
+import cpp_PCA as cpca
+import cpp_dispersion as cdisp
+import cpp_epochs as cpe
+import cpp_parameter_handlers as hand
+import cpp_plots as cplt
+import nems.recording as recording
+import nems_db.baphy as nb
 
 # multi electode array loading options
 options = {'batch': 310,
            'site': 'BRT037b',
-           'rasterfs': 100}
+           'rasterfs': 10}
 
 # todo this is creating a cache in charlies directory, move somewhere else!
 # gives the uri to a cached recording. if the cached data does not exists, creates it and saves it.
 
-Test = True
+Test = False
 
 if Test == True:
     # # sets automatic path finding
@@ -35,7 +32,6 @@ if Test == True:
 else:
     load_URI = nb.baphy_load_multichannel_recording(**options)
     loaded_rec = recording.load_recording(load_URI)
-
 
 # sets eps correpsondign to individual sounds as well as context probe pairs
 rec = cpe.set_recording_subepochs(loaded_rec, set_pairs=True)
@@ -60,10 +56,9 @@ if plot == True:
         ax.set_ylabel('cumulative explained variance');
         ax.set_title('PCA: fraction of variance explained')
 
-
     # selects most responsive celll
-    scat_key = {'s':5, 'alpha':0.5}
-    cplt.hybrid(sig, epoch_names='single', channels='all', start=0, end=3, scatter_kws=scat_key,)
+    scat_key = {'s': 5, 'alpha': 0.5}
+    cplt.hybrid(sig, epoch_names='single', channels='all', start=0, end=3, scatter_kws=scat_key, )
 
     good_cell_index = [5, 7, 8, 10, 11, 14]
 
@@ -80,22 +75,20 @@ if plot == True:
 
     # Initially plots statespace considering the most responsive units and then the PCs:
 
-    traj_kws = {'smoothing': 2, # some mild smoothing
-                'downsample': 10, # from 100 to 50 hz
+    traj_kws = {'smoothing': 2,  # some mild smoothing
+                'downsample': 10,  # from 100 to 50 hz
                 'rep_scat': False,
                 'rep_line': True,
                 'mean_scat': False,
                 'mean_line': True}
 
-
     # trajectory in neuron space of two good cells
     cplt.signal_trajectory(sig, dims=[5, 11], epoch_names=r'\AC\d_P3', _trajectory_kws=traj_kws)
 
-    cplt.signal_trajectory(sig_pca, dims=3, epoch_names=r'\AC\d_P3',_trajectory_kws=traj_kws)
-    cplt.signal_trajectory(sig_pca, dims=3, epoch_names='C0_P3',_trajectory_kws=traj_kws)
+    cplt.signal_trajectory(sig_pca, dims=3, epoch_names=r'\AC\d_P3', _trajectory_kws=traj_kws)
+    cplt.signal_trajectory(sig_pca, dims=3, epoch_names='C0_P3', _trajectory_kws=traj_kws)
 
-
-    cplt.signal_trajectory(sig_pca, dims=3, epoch_names=r'\AC0_P\d',_trajectory_kws=traj_kws)
+    cplt.signal_trajectory(sig_pca, dims=3, epoch_names=r'\AC0_P\d', _trajectory_kws=traj_kws)
     cplt.hybrid(sig, epoch_names=r'\AC0_P\d', channels='all', start=3, end=6, scatter_kws=scat_key)
 
 # the previous preliminary plots of state space show a lack of stereotipical trajectoryies, this and the
@@ -109,9 +102,7 @@ good_cell_names = [chan_name for cc, chan_name in enumerate(sig.chans) if cc in 
 filt_sig = sig.extract_channels(good_cell_names)
 filt_pca, PCA = cpca.signal_PCA(filt_sig, center=True)
 
-
 if plot == True:
-
     # variance explained by PCs
     fig, ax = plt.subplots()
     toplot = np.cumsum(PCA.explained_variance_ratio_)
@@ -123,51 +114,59 @@ if plot == True:
     # ... so far not more encouragig
 
     # full scatter pltot
-    scat_key = {'s':5, 'alpha':0.5}
+    scat_key = {'s': 5, 'alpha': 0.5}
     cplt.hybrid(filt_sig, epoch_names='single', channels='all', start=0, end=3, scatter_kws=scat_key)
     cplt.hybrid(filt_sig, epoch_names=r'\AC\d_P4', channels=4, start=3, end=6, scatter_kws=scat_key)
 
     # trajectory plots
     # 3 best units
-    traj_kws = {'smoothing': 2, # some mild smoothing
-                'downsample': 10, # from 100 to 50 hz
+    traj_kws = {'smoothing': 2,  # some mild smoothing
+                'downsample': 10,  # from 100 to 50 hz
                 'rep_scat': False,
                 'rep_line': True,
                 'mean_scat': False,
                 'mean_line': True}
-    cplt.signal_trajectory(filt_sig, dims=[0, 3, 4], epoch_names=r'\AC0_P\d',_trajectory_kws=traj_kws)
+    cplt.signal_trajectory(filt_sig, dims=[0, 3, 4], epoch_names=r'\AC0_P\d', _trajectory_kws=traj_kws)
     cplt.signal_trajectory(filt_pca, dims=3, epoch_names=r'\AC0_P\d', _trajectory_kws=traj_kws)
 
 
 
+
+############################## dispersion analyis
 # example of full population ploting for a set of all contexts to a probe
 epoch_names = r'\AC\d_P3'
 channels = 'all'
 
-# calculates kurskal wallis between different context across time
-disp_pval = cdisp.signal_single_cell_dispersion(sig, epoch_names=epoch_names, channels=channels)
-# defines significance, uses window size equal to time bin size
-time_window = 0.01 # 10 ms, this is actually the bin size,
-window = int(time_window * sig.fs)
-disp_sign = cdisp._significance_criterion(disp_pval, window=window, alpha=0.01) # array with shape
+# todo clean this function, move elsewhere
+def plot_single(channels, epochs, window_kws=None):
+    # calculates kurskal wallis between different context across time
+    disp_pval = cdisp.signal_single_cell_dispersion(sig, epoch_names=epochs, channels=channels, window_kws=window_kws)
+    # defines significance, uses window size equal to time bin size
+    insignificance = cdisp._significance_criterion(disp_pval, window=1, alpha=0.01)  # array with shape
 
-# overlays significatn times on the raster and PSTH for the specified cells and context probe pairs
-scat_key = {'s': 5, 'alpha': 0.5}
-cplt.hybrid(sig, epoch_names=epoch_names, channels=channels, start=3, end=6, scatter_kws=scat_key, significance=disp_sign)
+    # overlays significatn times on the raster and PSTH for the specified cells and context probe pairs
+    scat_key = {'s': 5, 'alpha': 0.5}
+    fig, ax = cplt.hybrid(sig, epoch_names=epochs, channels=channels, start=3, end=6, scatter_kws=scat_key,
+                          significance=insignificance)
 
+    return fig, ax
 
+fig, ax = plot_single(channels, epoch_names)
+fig.suptitle('full population example for probe 3')
 
+##############################
 # considers each combination of cell/probe as an independent recording (disregard cell identity)
 # 1 iterates over all relevant probes i.e. excludes silence
-# todo clean this function
-def population_significance(signal, channels, probes=[1, 2, 3, 4], sort=True):
-
+# todo clean this function, move elsewhere
+def population_significance(signal, channels, probes=[1, 2, 3, 4], sort=True, window_kws=None, hist=False, bins=60):
     all_probes = list()
     compound_names = list()
 
+    # calculates dipersion pval for eache set of contexts probe.
     for pp in probes:
         this_probe = r'\AC\d_P{}'.format(pp)
-        disp_mat = cdisp.signal_single_cell_dispersion(signal, epoch_names=this_probe, channels=channels)
+        disp_mat = cdisp.signal_single_cell_dispersion(signal, epoch_names=this_probe, channels=channels,
+                                                       window_kws=window_kws)
 
         chan_idx = hand._channel_handler(signal, channels)
         cell_names = [name for nn, name in enumerate(signal.chans) if nn in chan_idx]
@@ -179,89 +178,80 @@ def population_significance(signal, channels, probes=[1, 2, 3, 4], sort=True):
     compound_names = np.asarray(compound_names)
 
     # concatenates across first dimention i.e. cell/channel
-    pop_pval = np.concatenate(all_probes,axis=0)
+    pop_pval = np.concatenate(all_probes, axis=0)
 
     # defines significnce
     pop_sign = cdisp._significance_criterion(pop_pval, window=1, alpha=0.01)
     times = np.arange(0, pop_sign.shape[1]) / signal.fs
 
-    # set size of scatter markers
-    scat_kwargs = {'s': 10}
 
-    # raster significance, unsorted
-    if sort == False:
-        fig, ax = cplt._raster(times, pop_sign, scatter_kws=scat_kwargs)
-        ax.set_yticks(np.arange(0,pop_sign.shape[0],1))
-        ax.set_yticklabels(compound_names)
+    if hist == False:
+        # set size of scatter markers
+        scat_kwargs = {'s': 10}
+        # raster significance, unsorted
+        if sort == False:
+            fig, ax = cplt._raster(times, pop_sign, scatter_kws=scat_kwargs)
+            ax.set_yticks(np.arange(0, pop_sign.shape[0], 1))
+            ax.set_yticklabels(compound_names)
 
-    elif sort == True:
-        # organizes by last significant time for clarity
-        def sort_by_last_significant_bin(unsorted):
-            last_True = list()
-            for cell in range(unsorted.shape[0]):
-                # find last significant point
-                idxs = np.where(unsorted[cell, :] == True)[0]
-                if idxs.size == 0:
-                    idxs = 0
-                else:
-                    idxs = np.max(idxs)
-                last_True.append(idxs)
-            sort_idx = np.argsort(np.asarray(last_True))
+        elif sort == True:
+            # organizes by last significant time for clarity
+            def sort_by_last_significant_bin(unsorted):
+                last_True = list()
+                for cell in range(unsorted.shape[0]):
+                    # find last significant point
+                    idxs = np.where(unsorted[cell, :] == True)[0]
+                    if idxs.size == 0:
+                        idxs = 0
+                    else:
+                        idxs = np.max(idxs)
+                    last_True.append(idxs)
+                sort_idx = np.argsort(np.asarray(last_True))
 
-            # initializes empty sorted array
-            sorted_sign = np.empty(shape=unsorted.shape)
-            for ii, ss in enumerate(sort_idx):
-                sorted_sign[ii,:] = pop_sign[ss,:]
+                # initializes empty sorted array
+                sorted_sign = np.empty(shape=unsorted.shape)
+                for ii, ss in enumerate(sort_idx):
+                    sorted_sign[ii, :] = pop_sign[ss, :]
 
-            return sorted_sign, sort_idx
+                return sorted_sign, sort_idx
 
-        sorted_sign, sort_idx = sort_by_last_significant_bin(pop_sign)
+            sorted_sign, sort_idx = sort_by_last_significant_bin(pop_sign)
 
-        sorted_names = compound_names[sort_idx]
+            sorted_names = compound_names[sort_idx]
 
-        # rasters the sorted significances
+            # rasters the sorted significances
 
-        fig, ax = cplt._raster(times, sorted_sign, scatter_kws=scat_kwargs)
-        ax.set_yticks(np.arange(0,sorted_sign.shape[0],1))
-        ax.set_yticklabels(sorted_names)
-        ax.set_title('sorted significane')
+            fig, ax = cplt._raster(times, sorted_sign, scatter_kws=scat_kwargs)
+            ax.set_yticks(np.arange(0, sorted_sign.shape[0], 1))
+            ax.set_yticklabels(sorted_names)
 
+    elif hist == True:
+        _, sign_times = np.where(pop_sign == True)
+
+        fig, ax = plt.subplots()
+        ax.hist(sign_times, bins=bins)
     return fig, ax
 
 
 fig, ax = population_significance(sig, channels='all')
+fig.suptitle('all cells * all cpp')
+ax.axvline(3, color='red')
 
-# sanity check. a cell without any significant bins
-# todo clean this function
-def plot_single(channels, epochs):
-
-    # calculates kurskal wallis between different context across time
-    insig_pval = cdisp.signal_single_cell_dispersion(sig, epoch_names=epochs, channels=channels)
-    # defines significance, uses window size equal to time bin size
-    time_window = 0.01 # 10 ms, this is actually the bin size,
-    window = int(time_window * sig.fs)
-    insignificance = cdisp._significance_criterion(insig_pval, window=window, alpha=0.01) # array with shape
-
-    # overlays significatn times on the raster and PSTH for the specified cells and context probe pairs
-    scat_key = {'s': 5, 'alpha': 0.5}
-    fig, ax = cplt.hybrid(sig, epoch_names=epochs, channels=channels, start=3, end=6, scatter_kws=scat_key, significance=insignificance)
-
-    return fig, ax
-
+# sanity check 1. a cell without any significant bins
 insig_eps = r'\AC\d_P4'
 insig_cell = 'BRT037b-39-1'
 plot_single(insig_cell, insig_eps)
-
 
 # sanity check 2. plots the cell with the latest significant bin
 sign_eps = r'\AC\d_P4'
 sign_cell = 'BRT037b-06-1'
 plot_single(sign_cell, sign_eps)
 
-
+##############################
 # repeat the 'population sumary plots' but only keeping good cells.
 fig, ax = population_significance(sig, channels=good_cell_index, sort=True)
-ax.axvline(3)
+fig.suptitle('good cells * all cpps, significant difference over time')
+ax.axvline(3, color='red')
 
 # plots worst and best
 best_cell = 'BRT037b-46-1'
@@ -271,6 +261,35 @@ plot_single(best_cell, best_probe)
 worst_cell = 'BRT037b-31-1'
 worst_probe = r'\AC\d_P1'
 plot_single(worst_cell, worst_probe)
+
+##############################
+### calculates pvalues with a rolling window
+epoch_names = r'\AC\d_P3'
+channels = good_cell_index
+
+# calculates kurskal wallis between different context across time
+wind_kws = {'window': 5, 'rolling': True}
+
+# plots all cells for example cpp
+fig, ax = plot_single(channels=channels, epochs=epoch_names, window_kws=wind_kws)
+
+fig, ax = population_significance(sig, channels=channels, sort=True, window_kws=wind_kws)
+ax.axvline(3, color='red')
+fig.suptitle('all cells * all cpp, kruskal window = 5')
+
+# plots best
+sig_eps = r'\AC\d_P1'
+sig_cell = 'BRT037b-33-3'
+fig, ax =plot_single(sig_cell, sig_eps, wind_kws)
+fig.suptitle('latest significance')
+
+# plots wort
+insig_eps = r'\AC\d_P4'
+insig_cell = 'BRT037b-38-1'
+fig, ax = plot_single(insig_cell, insig_eps, wind_kws)
+fig.suptitle('earliest significance')
+
+
 
 
 
--- cpp_dispersion.py
+++ cpp_dispersion.py
@@ -4,11 +4,114 @@ from sklearn.metrics.pairwise import pairwise_distances
 
 import cpp_parameter_handlers as hand
 
+### helper funtions
 
-def _single_cell_dispersion(matrixes, channels='all'):
+def _into_windows(array, window, axis = -1, rolling=True, padding=np.nan,):
     '''
-    given a dictionary of matrices (from signal.extract_epochs), calculates pvalue for a Kruskal Wallis considering the response
-    to the different stimuli (different epochs, each of the keywords in the dictionary)
+    I am so proud of this fucntion. Takes an nd array, of N dimensions and generates an array of N+1 of windows across
+    the selected dimention. The selected dimention becomes window number, and the new last dimension becomes the original
+    dimention units across the window lenght.
+    :param array: nd array
+    :param window: window size in bins
+    :param axis: axis along which to generate the windows
+    :param rolling: wheter to use rollinge overlaping windows or static yuxtaposed windows
+    :param padding: element used to pad the last windows
+    :return: nd.array of n+1 dimensions,
+    '''
+    if window == 1:
+        windowed = np.expand_dims(array, axis=array.ndim)
+        return windowed
+    elif window == 0:
+        raise ValueError('window must be possitive non zero integer')
+
+    # swapps interest axis to last positions for easier indexing
+    swapped = np.swapaxes(array, axis1=axis, axis2=-1)
+    old_shape = swapped.shape
+
+    # defines paddign
+    pad_shape = list(old_shape)
+
+    if rolling == True:
+        pad_shape[-1] = pad_shape[-1] + window - 1
+    elif rolling == False:
+        extra_len = int((window * np.ceil(pad_shape[-1] / window)) - pad_shape[-1])
+        pad_shape[-1] = pad_shape[-1] + extra_len
+
+    padded = np.empty(pad_shape)
+    padded[:] = padding
+    padded[..., :swapped.shape[-1]] = swapped
+
+    if rolling == True:
+        newshape = list(old_shape)
+        newshape.append(window) # just add a new dimention of window length and the end
+        windowed = np.empty(newshape)
+
+        for ii in range(old_shape[-1]):
+            windowed[..., ii, :] = padded[..., ii:ii + window]
+
+    elif rolling == False:
+        old_ax_len = int(pad_shape[-1]/window) # windowed dimention divided by window len
+        newshape = pad_shape
+        newshape[-1] = old_ax_len # modifies old dimention
+        newshape.append(window) # add new dimention i.e. elements along window.
+        windowed = padded.reshape(newshape, order='C') # C order, changes las index the fastest.
+
+
+    # returs to the "original" dimention order, where the windowed dimention becomes the window dimention i.e. window number
+    # and the last dimension becomes the values corresponding to the selected dimnesion, along each window.
+    windowed = windowed.swapaxes(axis, -2) # swaps the original last dimension(-2) back in place
+
+    return windowed
+
+
+def _into_rolling_windows(array, window, axis, padding=np.nan): # todo deprecate
+
+    '''
+    makes a 2d array into a 3d array containing a colection of rolled windows in time
+    :param array: 2D array, with dimentions Channels x Time bins
+    :param window: window size in time bins
+    :param padding: value to fill the end of the rolling windows, nan is default
+    :return: 3d array with dimetions Channels x Windows x Time bins in window
+    '''
+
+    padded = np.empty([array.shape[0], array.shape[1] + window - 1])
+    padded[:] = padding
+    padded[:array.shape[0], :array.shape[1]] = array
+
+    windowed = np.empty([array.shape[0], array.shape[1], window])
+
+    for ii in range(array.shape[1]):
+        windowed[:, ii, :] = padded[:, ii:ii + window]
+
+    return windowed
+
+
+def _into_static_windows(array, window, padding=np.nan): #todo deprecate
+    '''
+    makes a 2d array into a 3d array containing a collection of succesive, non overlaping windows in time
+    :param array: 2d array, with dimentions Channels x Time bins
+    :param window: window size in time bins
+    :param padding: value to fill the last windows, nan is default
+    :return: 3d array with dimentions Channels x Windows x Time bins in window
+    '''
+
+    extra_len = int((window * np.ceil(array.shape[1]/window)) - array.shape[1])
+
+    padded = np.empty([array.shape[0], array.shape[1] + extra_len])
+    padded[:] = padding
+    padded[:array.shape[0], :array.shape[1]] = array
+
+    windowed = padded.reshape(array.shape[0], int(padded.shape[1]/window), window)
+
+    return windowed
+
+
+### base dispersion fucntions
+
+def _single_cell_dispersion(matrixes, channels='all', window=1, rolling=False):
+    '''
+    given a dictionary of matrices (from signal.extract_epochs), calculates pvalue for a Kruskal Wallis considering
+    the response to the different stimuli (different epochs, each of the keywords in the dictionary)
     . these calculations are done over time i.e. for each time bin
     :param matrix: a dictionary of matrices of dimensions Repetitions x Cells x Time.
                    Each keywords corresponds to a different stimulus
@@ -16,19 +119,29 @@ def _single_cell_dispersion(matrixes, channels='all'):
     :return: an array of shape Cell x Time, of pvalues for each cell across time.
     '''
     # stacks all matrixes (different vocalizations) across new axis, then selects the desired cell
-    full_mat = np.stack(matrixes.values(), axis=3)
+    full_mat = np.stack(matrixes.values(), axis=3) # shape: Repetitions x Channels x TimeBins x ContextStimuli
 
     # handles channel keywords
     channels = hand._channel_handler(full_mat[..., 0], channels)
 
-    # initializes result matrix with shape C x T where C is cell and T is time
-    shape = full_mat.shape
+    # generates the windowed array
+
+    windowed = _into_windows(full_mat, window=window, axis=2, rolling=rolling)
+
+
+    # initializes result matrix with shape C x T where C is cell and T is time or TimeWindow
+    shape = windowed.shape
     kruscal_over_time = np.zeros([len(channels), shape[2]])  # empty array of dimentions Cells x Time
 
     # iterates over cell
     for cc, cell in enumerate(channels):
+        # iterates over time window
         for time in range(shape[2]):
-            working_slice = full_mat[:, cell, time, :].T  # the resulting array has dimentions Context x Repetition
+            working_slice = windowed[:, cell, time, :,:]  # array has dimentions Repetition x Context x winndow Time bin
+            # flattens the window time bin dimension into the Repetition dimension
+            working_slice = np.swapaxes(working_slice, 0, 1)
+            work_shape = working_slice.shape
+            working_slice = working_slice.reshape(work_shape[0], work_shape[1] * work_shape[2])
 
             try:
                 kruscal = sst.kruskal(*working_slice)
@@ -52,20 +165,8 @@ def _significance_criterion(pvalues, window=1, alpha=0.01):
     :return: boolean array of the same dimentions of pvalues array
     '''
 
-    # def rolling_window(a, window):
-    #     shape = a.shape[:-1] + (a.shape[-1] - window + 1, window)
-    #     strides = a.strides + (a.strides[-1],)
-    #     return np.lib.stride_tricks.as_strided(a, shape=shape, strides=strides)
-
-    # padds across time with nan
-    padded = np.empty([pvalues.shape[0], pvalues.shape[1] + window - 1])
-    padded[:] = np.nan
-    padded[:pvalues.shape[0], :pvalues.shape[1]] = pvalues
-
-    windowed = np.empty([pvalues.shape[0], pvalues.shape[1], window])
-
-    for ii in range(pvalues.shape[1]):
-        windowed[:, ii, :] = padded[:, ii:ii + window]
+    # windowed = _into_rolling_windows(pvalues, window, padding=np.nan)
+    windowed = _into_windows(pvalues, window=window, axis=1, rolling=True, padding=np.nan)
 
     sign_bin = np.where(windowed <= alpha, True, False)  # which individual time bins are significant
 
@@ -150,7 +251,12 @@ def _pairwise_distance_between(matrixes):
 
 ### signal wrapers
 
-def signal_single_cell_dispersion(signal, epoch_names='single', channels='all'):
+def signal_single_cell_dispersion(signal, epoch_names='single', channels='all', window_kws=None):
+
+    # window keywords
+    window_kws = {} if window_kws is None else window_kws
+
+
     # handles epoch_names as standard
     epoch_names = hand._epoch_name_handler(signal, epoch_names)
 
@@ -159,7 +265,7 @@ def signal_single_cell_dispersion(signal, epoch_names='single', channels='all'):
 
     matrixes = signal.rasterize().extract_epochs(epoch_names)
 
-    disp = _single_cell_dispersion(matrixes, channels=channels)
+    disp = _single_cell_dispersion(matrixes, channels=channels, **window_kws)
 
     return disp
 
