60b42caf4ee4ec5e5bf7be7eda3b065714568bd9
Mateo Lopez Espejo <lopezesp@ohsu.edu>
2020-11-21 16:44:24 -0800

    create small function to pull all the name files as needed by the integration_quilt program in matlab

--- scripts/4_sam_data/200107_import_NTI.py
+++ scripts/4_sam_data/200107_import_NTI.py
@@ -1,5 +1,6 @@
 import nems.recording as recording
 import nems_lbhb.baphy as nb
+import collections as coll
 from nems import db as nd
 from src.data.nti_epochs import set_recording_subepochs
 
@@ -11,7 +12,8 @@ in short, this I should be alble of import the data with the appropriate tags, s
 pairs to use my integration analysis approach
 '''
 
-batch = 319  # NTI batch, Sam paradigm
+batch = 318  # NTI in A1
+batch = 319  # NTI in PEG
 
 # check sites in batch
 batch_cells = nd.get_batch_cells(batch)
@@ -21,17 +23,38 @@ site_ids = set([cellid.split('-')[0] for cellid in cell_ids])
 # for site in site_ids:
 site = 'AMT028b'
 options = {'batch': batch,
-           'siteid': site,
+           'cellid': site,
            'stimfmt': 'envelope',
            'rasterfs': 100,
            'recache': False,
            'runclass': 'NTI',
            'stim': False}
-load_URI = nb.baphy_load_recording_uri(**options)
+load_URI, _ = nb.baphy_load_recording_uri(**options)
 loaded_rec = recording.load_recording(load_URI)
 epochs = loaded_rec.epochs
 
 NTI_rec = set_recording_subepochs(loaded_rec)
+
+
+def get_NTI_files(batch):
+
+    batch_cells = nd.get_batch_cells(batch=batch)
+
+    site_dict = coll.defaultdict(list)
+    for cellid in batch_cells.cellid:
+        site_dict[cellid[:7]].append(cellid)
+
+    sites_files = list()
+    for site, cells in site_dict.items():
+        fdb = nd.get_cell_files(cellid=cells[0], runclass='NTI')
+        filename = fdb.respfile.values[0].split('.')[0]
+        sites_files.append(filename)
+
+    return sites_files
+
+print('\nA1 files\n', get_NTI_files(318))
+print('\nPEG files\n', get_NTI_files(319))
+
 
 '''
 script finished, relevant functions have bee wrapped and moved to nti_epochs.py
