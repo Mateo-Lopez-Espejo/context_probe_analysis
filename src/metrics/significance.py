8f736a14a9b2a09fc42cc8f6c2a5806b34a92686
Mateo <mateo.lopez.espejo@gmail.com>
2021-02-28 12:07:52 -0800

    1. add funtion to find streches of consecutive true values in significance. 2. add new significance corrections counting for significant chunks

--- src/metrics/significance.py
+++ src/metrics/significance.py
@@ -3,17 +3,78 @@ from configparser import ConfigParser
 from src.metrics.consolidated_metrics import _append_means_to_array
 import numpy as np
 import numpy.ma as ma
+import operator
 
-def  _significance(array, mont_array, multiple_comparisons_axis=None, alpha=0.01, tails='both'):
+def where_contiguous_chunks(array, axis, length, func='>='):
+    """
+    find the indices where a contiguous number of True values along axis are. finds True chunks equal or longer than the
+    specified number
+    :param array: boolean array
+    :param axis: int, axis over which to count for contiguous True
+    :param length: chunks of length equal or greater are selected
+    :func: the comparison between the chunk lengths and desired length
+    :return: tuple of indices
+    """
+
+    # sanitizes input
+    if axis == -1: axis = array.ndim-1
+
+    # find the start and end of True chunks
+    d = np.diff(array.astype(int), axis=axis, prepend=0, append=0)
+    starts = np.where(d == 1)
+    stops = np.where(d == -1)
+
+    # counts the True chunks and select those with adecuate length
+    ops = {'>': operator.gt, '<': operator.lt, '>=': operator.ge, '<=': operator.le, '==': operator.eq}
+    good_chunks = ops[func](stops[axis] - starts[axis], length)
+
+    # save the indices from the other dimensions
+    if array.ndim > 1:
+        other_dims_idx = list(starts)
+        other_dims_idx.pop(axis)
+        other_dims_idx =  np.asarray(other_dims_idx)
+
+    # iterates over the selected chunks and defines indices for the chunk dimension and the extra dimensions
+    chunk_dim_idx = list()
+    extra_dim_idx = list()
+    for gc in np.where(good_chunks)[0]:
+        start = starts[axis][gc]
+        stop = stops[axis][gc]
+
+        # make contiguous indices for the chunk dimension
+        chunk_dim_idx.append(np.arange(start, stop))
+
+        # repeat chunk length times the indices for the other dimensions
+        if array.ndim > 1:
+            extra_dim_idx.append(np.repeat(other_dims_idx[:,[gc]], repeats=stop-start, axis=1))
+
+    chunk_dim_idx = np.concatenate(chunk_dim_idx, axis=0)
+
+    if array.ndim > 1:
+        extra_dim_idx = np.concatenate(extra_dim_idx, axis=1)
+
+        # places the chunk dimension indices alongside the other dimensions in a tuple in the original position
+        chunk_idx = list(extra_dim_idx.__iter__())
+        chunk_idx.insert(axis, chunk_dim_idx)
+        chunk_idx = tuple(chunk_idx)
+    else:
+        chunk_idx = (chunk_dim_idx,)
+
+    return chunk_idx
+
+def  _significance(array, mont_array, multiple_comparisons_axis=None, consecutive=None, alpha=0.01, tails='both'):
     """
     calculates significance (boolean) for the values of array using the montecarlo method e.g. n simulations or shuffles of the
     original data in array. These n repetitions are specified in the mont_array, therefore mont_array should have the
     same shape as array plus an aditional first dimension for said repetitions. Correction for multiple comparisons is
-    performed across the specified axies. alpha defines the threshold for considering pvalues as significant. tails
-    specify wheter calculate a one tailed (upper or lower) or two tailed pvalue.
+    performed across the specified axies. alpha defines the threshold for considering pvalues as significant.
+    If consecutive is an integer and multiple_comparisons_axis is a singleton, counts consecutive True values instead
+    of perfoming the multiple comparisons. Tails specify wheter calculate a one tailed (upper or lower) or two tailed
+    pvalue.
     :param array: ndarray e.g. Unit x Context x ...
     :param mont_array: ndarray e.g. Montecarlo x Unit x Context ...
     :param multiple_comparisons_axis: None, list of ints. default None.
+        :param consecutive: int, default None. If int, multiple_comparisons_axis must be a singleton
     :param alpha: float [0:1]
     :param tails: str, 'lesser', 'Greater', 'Both'
     :return:
@@ -32,18 +93,31 @@ def  _significance(array, mont_array, multiple_comparisons_axis=None, alpha=0.01
 
     significance = pvalues <= alpha
 
+    # does corrections, otherwise passe the raw significance
     if multiple_comparisons_axis is not None:
+        # counts consecutive True values
+        if isinstance(consecutive, int):
+            if len(multiple_comparisons_axis) != 1:
+                raise ValueError('when counting consecutive True, multiple_comparisons_axis must be singleton')
 
-        n_comparisons = np.prod(np.asarray(significance.shape)[np.asarray(multiple_comparisons_axis)])
-        n_chance_comp = np.ceil(n_comparisons * alpha)
+            chunk_idx = where_contiguous_chunks(significance, multiple_comparisons_axis[0], consecutive, func='>=')
+            chunk_signif = np.full_like(significance)
+            chunk_signif[chunk_idx] = True
+            significance = chunk_signif
 
-        # count the number of significant bins pooling acrooss the multiple comparisons axis, this is done independently
-        # across all the other non specified axis, e.g. the Unit dimension
-        sig_count = np.sum(significance, axis=tuple(multiple_comparisons_axis), keepdims=True)
+        # corrects for multiple comparisons
+        elif consecutive is None:
 
-        # creates a corrected significance by taking the significant bins of a groups of multiple comparisons, if the sum
-        # of significant bins is over the chance comparisons threshold
-        significance = np.where(sig_count > n_chance_comp, significance, np.full(significance.shape, False))
+            n_comparisons = np.prod(np.asarray(significance.shape)[np.asarray(multiple_comparisons_axis)])
+            n_chance_comp = np.ceil(n_comparisons * alpha)
+
+            # count the number of significant bins pooling acrooss the multiple comparisons axis, this is done independently
+            # across all the other non specified axis, e.g. the Unit dimension
+            sig_count = np.sum(significance, axis=tuple(multiple_comparisons_axis), keepdims=True)
+
+            # creates a corrected significance by taking the significant bins of a groups of multiple comparisons, if the sum
+            # of significant bins is over the chance comparisons threshold
+            significance = np.where(sig_count > n_chance_comp, significance, np.full(significance.shape, False))
 
     # defines the confidence intervals as the top and bottom percentiles summing to alpha
     low = alpha * 100
