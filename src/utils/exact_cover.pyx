fc68835f25ddd0ce59638dadb784d5b1075360fd
Mateo Lopez Espejo <lopezesp@ohsu.edu>
2021-04-19 18:40:08 -0700

    minor refactorign of files, penetration maps for Teonancatl, improvement on the exact cover script

--- src/utils/exact_cover.pyx
+++ src/utils/exact_cover.pyx
@@ -1,5 +1,6 @@
 from itertools import product, permutations
 from math import factorial
+import numpy as np
 
 def formulate_ctx_prb(n_vals):
     ind_val = range(1, n_vals + 1)
@@ -24,6 +25,33 @@ def formulate_ctx_prb(n_vals):
                 Y[perm_str].append(f'{p0}_{p1}')
 
     # creates a dictionary of definine in which sequences each pair is contained
+    X = {f'{p[0]}_{p[1]}': set() for p in pairs}
+    for seq in Y:
+        for pair in Y[seq]:
+            X[pair].add(seq)
+
+    return X, Y
+
+def formulate_ctx_prb_2(n_vals):
+    ind_val = range(1, n_vals + 1)
+    # defines universe of pairs
+    pairs = ([(0, i) for i in ind_val])  # sound afte silence
+    pairs.extend(list(permutations(ind_val, r=2)))  # sound pairs
+    print(f'{len(pairs)} pairs, {factorial(n_vals)} sequences')
+
+    # create all possible sequences of n values, whith one of them contiguosly duplicated
+    Y = dict()  # different format of all_seq pairs
+    for perm in permutations(ind_val):
+        perm = list(perm)  # inserts duplicated sound
+        perm.insert(0, 0)  # inserts leading silence
+        perm_str = '_'.join(str(p) for p in perm)
+
+        # finds which pairs this permutations contains and fills coverage matrix
+        Y[perm_str] = list()
+        for p0, p1 in zip(perm[:-1], perm[1:]):
+            Y[perm_str].append(f'{p0}_{p1}')
+
+    # creates a dictionary defining in which sequences each pair is contained
     X = {f'{p[0]}_{p[1]}': set() for p in pairs}
     for seq in Y:
         for pair in Y[seq]:
