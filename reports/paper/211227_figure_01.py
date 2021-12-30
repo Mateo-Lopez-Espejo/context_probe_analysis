e53c8f24cc45159562338e64bb68674ab752d7c9
Mateo Lopez Espejo <lopezesp@ohsu.edu>
2021-12-29 19:28:04 -0800

    add box over selected example sound on sound sequences. add sites to TNC left hemisphere map

--- reports/paper/211227_figure_01.py
+++ reports/paper/211227_figure_01.py
@@ -4,6 +4,7 @@ from math import pi
 
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 import seaborn as sns
 import pandas as pd
 import  joblib as jl
@@ -22,6 +23,7 @@ colors = ['blue', 'orange', 'green', 'purple', 'brown']
 dummy_wave = np.sin(np.linspace(0,pi*4,n_samps)) # todo, pull some real example waves??
 waves = [np.zeros(n_samps)] + [dummy_wave, ] * 5 + [np.zeros(n_samps)]
 verrical_offset = 2
+rect_idx = 2 # sound around wich draw rectangles
 
 
 fig, ax = plt.subplots()
@@ -29,10 +31,25 @@ fig, ax = plt.subplots()
 for ss, seq in enumerate(sequences):
 
     for ww, wave_idx in enumerate(seq):
+        # wave form plots
         x = np.linspace(0,1,n_samps) + ww
         y = waves[wave_idx] + ss * verrical_offset
         color = colors[wave_idx]
         ax.plot(x, y, color)
+
+        # vertical lines for clear separation of sounds
+        if ww > 0:
+            ax.axvline(ww, color='black', linestyle=':')
+
+        # add rectangle to point at exaample
+        if wave_idx == rect_idx:
+            rect_x = ww - 1
+            rect_y = ss * verrical_offset - 1
+            rect_w, rect_h = 2, 2 # 2 seconds widht, 2*norm wave
+            rect = patches.Rectangle((rect_x, rect_y), rect_w, rect_h,
+                                     linewidth=3, edgecolor='black', facecolor='none')
+            ax.add_patch(rect)
+
 
 
 
